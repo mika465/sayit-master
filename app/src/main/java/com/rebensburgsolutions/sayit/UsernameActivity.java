8244fb54523d34bb6130b7c5b0e63535c807633a
mika465 <mika@rebensburg.net>
2017-05-19 17:46:10 +0200

    test
    test

---
+++ app/src/main/java/com/rebensburgsolutions/sayit/UsernameActivity.java
@@ -0,0 +1,30 @@
+package com.rebensburgsolutions.sayit;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class UsernameActivity extends AppCompatActivity {
+
+    public Button button_go_username;
+
+    public void init(){
+        button_go_username = (Button)findViewById(R.id.button_username);
+        button_go_username.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent openLobbyActivity = new Intent(UsernameActivity.this, LobbyActivity.class);
+                startActivity(openLobbyActivity);
+            }
+        });
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_username);
+        init();
+    }
+}
